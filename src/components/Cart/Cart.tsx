d7850639a20f9a6927f853e2bfba892292d78cd5
Volodymyr <vvas1@softserveinc.com>
2021-12-29 11:20:04 +0200

    removed hardcode

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -13,10 +13,8 @@ const Cart: FC<{}> = (): React.ReactElement => {
   return (
     <div tabIndex={0} onBlur={()=> setShow(false)} className={CartModule['cart']}>
       <span onClick={clickHandler} className={`${CartModule['cart-button']} ${show ? CartModule['cart-button-active']: ""}`}>{state.cart.items.length} pizza{state.cart.items.length > 1 || state.cart.items.length === 0?"s":""}</span>
-      <div className={true?CartModule['show']:CartModule['hide']}>
+      <div className={show ? CartModule['show']:CartModule['hide']}>
         <ul className={CartModule['cart-list']}>
-          <li onClick={clickHandler} className={CartModule['list-item']}>Napoletana</li>
-          <li onClick={clickHandler} className={CartModule['list-item']}>Caprese</li>
           {state.cart.items.map(item=>{
             return <li key={item.id} onClick={clickHandler} className={CartModule['list-item']}><div style={{display:"flex",width:'100%'}}>{item.name} x {item.quantity}</div></li>
           })}
